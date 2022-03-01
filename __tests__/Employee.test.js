b395821d91c46c210f821e58abae5cf836adafcd
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-01 15:43:26 -0800

    employee test - red

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,14 @@
 
+const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employee.js");
+
+// make new employee class
+test("create new Employee class", () => {
+    const employee = new Employee("Dave");
+
+    expect(employee.name).toBe("Dave");
+    expect(employee.id).toEqual(expect.any(Number));
+
+    const email = "google@gmail.com";
+    expect(email).toEqual(expect.stringMatching("@"));
+})
