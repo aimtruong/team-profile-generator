098e4ea36537dd7c98f73ab71f663634c68c370c
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-03 17:04:32 -0800

    prompts go through

---
+++ index.js
@@ -0,0 +1,141 @@
+
+const inquirer = require("inquirer");
+
+const manInfo = () => {
+    // if no "managers" array
+    var managers = [];
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "manName",
+            message: "What is the manager's name? "
+        },
+        {
+            type: "input",
+            name: "manId",
+            message: "What is the manager's employee ID? "
+        },
+        {
+            type: "input",
+            name: "manEmail",
+            message: "What is the manager's email? "
+        },
+        {
+            type: "input",
+            name: "manOffice",
+            message: "What is the manager's office number? "
+        },
+        {
+            type: "confirm",
+            name: "confirmManInfo",
+            message: "Would you like to add another manager? ",
+            default: false
+        }
+    ])
+    .then(manData => {
+        managers.push(manData);
+        if(manData.confirmManInfo){
+            return manInfo(managers);
+        }
+        else{
+            return managers;
+        }
+    });
+};
+
+const engInfo = (engineers) => {
+    var engineers = [];
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "engName",
+            message: "What is the engineer's name? "
+        },
+        {
+            type: "input",
+            name: "engId",
+            message: "What is the engineer's employee ID? "
+        },
+        {
+            type: "input",
+            name: "engEmail",
+            message: "What is the engineer's email? "
+        },
+        {
+            type: "input",
+            name: "engGit",
+            message: "What is the engineer's GitHub username? "
+        },
+        {
+            type: "confirm",
+            name: "confirmEngInfo",
+            message: "Would you like to add another engineer? ",
+            default: false
+        }
+    ])
+    .then(engData => {
+        engineers.push(engData);
+        if(engData.confirmEngInfo){
+            return engInfo(engineers);
+        }
+        else{
+            return engineers;
+        }
+    });
+};
+
+const intInfo = (interns) => {
+    // if no 'interns' array
+    if(!interns.int){
+        interns.int = [];
+    };
+
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "intName",
+            message: "What is the intern's name? "
+        },
+        {
+            type: "input",
+            name: "intId",
+            message: "What is the intern's employee ID? "
+        },
+        {
+            type: "input",
+            name: "intEmail",
+            message: "What is the intern's email? "
+        },
+        {
+            type: "input",
+            name: "intSchool",
+            message: "Where did the intern graduated? "
+        },
+        {
+            type: "confirm",
+            name: "confirmIntInfo",
+            message: "Would you like to add another intern? ",
+            default: false
+        }
+    ])
+    .then(intData => {
+        interns.push(intData);
+        if(intData.confirmIntInfo){
+            return intInfo(interns);
+        }
+        else{
+            return interns;
+        }
+    });
+};
+
+manInfo()
+    .then(engInfo)
+    .then(intInfo)
+    //.then(
+    //    console.log(manInfo),
+    //    console.log(engInfo),
+    //    console.log(intInfo)
+   // );
