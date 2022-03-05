4bd16bc303db498592e55ad87daee3d939437a28
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-05 15:48:21 -0800

    page layout complete with HTML and CSS

--- src/page-template.js
+++ src/page-template.js
@@ -1,50 +1,49 @@
 
 const generateEngineer = engineers => {
     return `
-        <section class = "card">
-            <div class = "job-title">
-            ${engineers
-                .map(({ engName, engId, engEmail, engGit }) => {
-                    return `
+    ${engineers
+        .map(({ engName, engId, engEmail, engGit }) => {
+            return `
+                <section class = "card">
+                    <div class = "job-title">
                         <p>${engName}</p>
-                        <p>Engineer</p>
-                        </div>
+                        <p><i class="bi bi-laptop-fill"></i>    Engineer</p>
+                    </div>
 
-                        <div class = "job-detail">
-                            <p class = "job-info">ID: ${engId}</p>
-                            <p class = "job-info">Email: ${engEmail}</p>
-                            <p class = "job-info">GitHub: ${engGit}</p>
-                        </div>
-                        </section>
-                    `
-                })
-                .join("")
-            }
+                    <div class = "job-detail">
+                        <p class = "job-info">ID: ${engId}</p>
+                        <p class = "job-info">Email: <a href = "mailto:${engEmail}">${engEmail}</a></p>
+                        <p class = "job-info">GitHub: <a href = "github.com/${engGit}" target = "_blank">${engGit}</a></p>
+                    </div>
+                </section>
+            `
+        })
+        .join("")
+    }
     `;
 };
 
 const generateIntern = interns => {
     return `
-        <section class = "card">
-            <div class = "job-title">
-            ${interns
-                .map(({ intName, intId, intEmail, intSchool }) => {
-                    return `
-                    <p>${intName}</p>
-                    <p>Intern</p>
+    ${interns
+        .map(({ intName, intId, intEmail, intSchool }) => {
+            return `
+                <section class = "card">
+                    <div class = "job-title">
+                        <p>${intName}</p>
+                        <p><i class="bi bi-mortarboard-fill"></i>   Intern</p>
                     </div>
 
                     <div class = "job-detail">
                         <p class = "job-info">ID: ${intId}</p>
-                        <p class = "job-info">Email: ${intEmail}</p>
+                        <p class = "job-info">Email: <a href = "mailto:${intEmail}">${intEmail}</a></p>
                         <p class = "job-info">School: ${intSchool}</p>
                     </div>
-                    </section>
-                    `
-                })
-                .join("")
-            }
-                    
+                </section>
+            `
+        })
+        .join("")
+    }
     `;
 };
 
@@ -60,6 +59,7 @@ module.exports = answers => {
             <meta http-equiv = "X-UA-Compatible" content = "ie=edge">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
             <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
             <link rel="stylesheet" href="style.css">
             <title>Team Profile Generator</title>
         </head>
@@ -74,11 +74,11 @@ module.exports = answers => {
                 <section class = "card">
                     <div class = "job-title">
                         <p>${manName}</p>
-                        <p>Manager</p>
+                        <p><i class="bi bi-clipboard-check-fill"></i>   Manager</p>
                     </div>
                     <div class = "job-detail">
                         <p class = "job-info">ID: ${manId}</p>
-                        <p class = "job-info">Email: ${manEmail}</p>
+                        <p class = "job-info">Email: <a href = "mailto:${manEmail}" >${manEmail}</a></p>
                         <p class = "job-info">Office number: ${manOffice}</p>
                     </div>
                 </section>
